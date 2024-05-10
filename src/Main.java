77c39e5623599d0a5665b396b50b4d584539235e
Miguel-Jaber <128867836+Miguel-Jaber@users.noreply.github.com>
2024-05-10 17:28:57 -0300

    Mensagens e quebras de linha

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,22 @@ public class Main {
         try {
             float valorProduto = Float.parseFloat(showInputDialog(null, "Digite o valor do produto:"));
             float valorPago = Float.parseFloat(showInputDialog(null, "Digite o valor pago:"));
-
+            
             float troco = valorPago - valorProduto;
 
             NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
 
-            if (valorProduto == valorPago) {
-                showMessageDialog(null, "Compra concluída com sucesso!");
-            } else if (valorProduto > valorPago) {
+            String mensagem = "Compra concluída com sucesso!";
+
+            mensagem += "\nValor do produto: " + currencyFormat.format(valorProduto);
+            mensagem += "\nSeu troco é: " + currencyFormat.format(troco);
+
+            if (valorPago < valorProduto) {
                 showMessageDialog(null, "Pagamento insuficiente!");
             } else {
-                showMessageDialog(null, "Compra concluída com sucesso! Seu troco é R$" + currencyFormat.format(troco));
+                showMessageDialog(null, mensagem);
             }
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             showMessageDialog(null, "Por favor, insira valores numéricos válidos.");
         }
     }
