c82adbeeb2756bd27c8a05c5673a5931274c6cf2
Miguel-Jaber <128867836+Miguel-Jaber@users.noreply.github.com>
2024-05-10 14:16:59 -0300

    Pagamento e troco funcionais

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class Main {
     public static void main(String[] args) {
-    
+        try {
+            float valorProduto = Float.parseFloat(showInputDialog(null, "Digite o valor do produto:"));
+            float valorPago = Float.parseFloat(showInputDialog(null, "Digite o valor pago:"));
 
+            float troco = valorPago - valorProduto;
+
+            if (valorProduto == valorPago) {
+                showMessageDialog(null, "Compra concluída com sucesso!");
+            } else if (valorProduto > valorPago) {
+                showMessageDialog(null, "Pagamento insuficiente!");
+            } else {
+                showMessageDialog(null, "Compra concluída com sucesso! Seu troco é R$" + troco);
+            }
+        }
+        catch (NumberFormatException e) {
+            showMessageDialog(null, "Por favor, insira valores numéricos válidos.");
+        }
     }
 }
