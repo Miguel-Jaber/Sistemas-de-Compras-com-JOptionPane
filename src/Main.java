72d191c6a84895472b348ee9f6ddce57678c952e
Miguel-Jaber <128867836+Miguel-Jaber@users.noreply.github.com>
2024-05-10 17:43:18 -0300

    Desconto de 10% nas compras acima de R00,00

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,31 @@ public class Main {
             float valorProduto = Float.parseFloat(showInputDialog(null, "Digite o valor do produto:"));
             float valorPago = Float.parseFloat(showInputDialog(null, "Digite o valor pago:"));
 
+            final float percentualDesconto1 = 0.10f;
+            float valorDesconto = 0.0f;
+
+            if (valorProduto > 300.0f) {
+                valorDesconto = valorProduto * percentualDesconto1;
+                valorProduto -= valorDesconto;
+            }
+
             float troco = valorPago - valorProduto;
 
             NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
 
-            if (valorProduto == valorPago) {
-                showMessageDialog(null, "Compra concluída com sucesso!");
-            } else if (valorProduto > valorPago) {
+            String mensagem = "Compra concluída com sucesso!";
+            if (valorDesconto > 0) {
+                mensagem += "\nDesconto aplicado: " + currencyFormat.format(valorDesconto);
+            }
+            mensagem += "\nValor do produto: " + currencyFormat.format(valorProduto);
+            mensagem += "\nSeu troco é: " + currencyFormat.format(troco);
+
+            if (valorPago < valorProduto) {
                 showMessageDialog(null, "Pagamento insuficiente!");
             } else {
-                showMessageDialog(null, "Compra concluída com sucesso! Seu troco é R$" + currencyFormat.format(troco));
+                showMessageDialog(null, mensagem);
             }
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             showMessageDialog(null, "Por favor, insira valores numéricos válidos.");
         }
     }
